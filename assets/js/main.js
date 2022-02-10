c5df54cd19673b36a364d7ea47821335ee6312b8
vtorresm <vatorresm88@gmail.com>
2022-02-09 23:24:35 -0500

    Skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,66 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-  navToggle.addEventListener('click', () =>{
-    navMenu.classList.add('show-menu')
-  })
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
+if (navClose) {
   navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
-  })
+    navMenu.classList.remove('show-menu');
+  });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show-menu')
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
 }
 
-navLink.forEach(link => link.addEventListener('click', linkAction))
+navLink.forEach((link) => link.addEventListener('click', linkAction));
 
 /*==================== SCROLL TO TOP ====================*/
 
-
 /*==================== ACCORDION SKILLS ====================*/
+const skilsContent = document.getElementsByClassName('skills__content'),
+  skillsHeader = document.querySelectorAll('.skills__header');
 
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skilsContent.length; i++) {
+    skilsContent[i].className = 'skills__content skills__close';
+  }
+
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open';
+  }
+}
+
+skillsHeader.forEach((header) => header.addEventListener('click', toggleSkills));
 
 /*==================== QUALIFICATION TABS ====================*/
 
-
 /*==================== SERVICES MODAL ====================*/
-
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-
 /*==================== TESTIMONIAL ====================*/
-
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
-
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
-
 /*==================== SHOW SCROLL UP ====================*/
-
 
 /*==================== DARK LIGHT THEME ====================*/
