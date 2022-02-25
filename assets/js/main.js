c6427d5a02259b2e099446036aff40927febc01d
vtorresm <vatorresm88@gmail.com>
2022-02-25 14:38:40 -0500

    Scroll Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,5 +161,12 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  if (this.scrollY >= 100) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
